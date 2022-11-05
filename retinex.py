8de0214feb23c6794fad3ba7213c579154349dae
unknown <joo>
2022-11-06 00:16:08 +0900

    modified retinex

--- retinex.py
+++ retinex.py
@@ -1,10 +1,12 @@
 import cv2
 import numpy as np 
-from normalize import clip3ch
+from normalize import clip3ch,clip
 
 def ssr(img, sigma):
+    # w의 값에 따라 compression level이 변함
     blur = cv2.GaussianBlur(img, (0,0), sigma)    
-    return  np.log(img + 1) - np.log(blur + 1)
+    w = 10
+    return  np.log10(img * w + 1) - np.log10(blur * w + 1)
 
 def msr(img, sigmas=[15,80,250]):
 
@@ -20,9 +22,13 @@ if __name__ =='__main__':
     img = cv2.imread('./images/desk.jpg')
     rows, cols = img.shape[:2]
     img = cv2.resize(img, (cols//4, rows//4))
-    out = clip3ch(msr(img))
+    # b,g,r 채널마다 clip
+    out3ch = clip3ch(msr(img))
+    # bgr 전체 채널에 대하여 clip
+    out = clip(msr(img))
 
     cv2.imshow('input', img)
+    cv2.imshow('retinex_clip3ch', out3ch)
     cv2.imshow('retinex', out)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
